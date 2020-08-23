2edd59d9fe976550e5016cc62057990263175e36
Sokos6 <will.sokolowski@gmail.com>
2020-08-23 16:04:35 -0500

    data setup

---
+++ src/utils/_DATA.js
@@ -0,0 +1,22 @@
+let phones = {
+  Techno: {
+    brand: 'Techno',
+    inCart: 'false', // or "in"
+  },
+  Apple: {
+    brand: 'Iphone',
+    inCart: 'false',
+  },
+  Samsung: {
+    brand: 'Samsung',
+    inCart: 'false',
+  },
+  Hauwei: {
+    brand: 'Hauwei',
+    inCart: 'true',
+  },
+  HTC: {
+    brand: 'HTC',
+    inCart: 'true',
+  },
+};
---
+++ src/utils/dist/_DATA.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var phones = {
+  Techno: {
+    brand: 'Techno',
+    inCart: 'false' // or "in"
+
+  },
+  Apple: {
+    brand: 'Iphone',
+    inCart: 'false'
+  },
+  Samsung: {
+    brand: 'Samsung',
+    inCart: 'false'
+  },
+  Hauwei: {
+    brand: 'Hauwei',
+    inCart: 'true'
+  },
+  HTC: {
+    brand: 'HTC',
+    inCart: 'true'
+  }
+};
